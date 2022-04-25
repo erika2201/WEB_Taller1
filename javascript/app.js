7613ea24fb55938a9e3df04859a86223bf866456
erika2201 <erikapapamija@gmail.com>
2022-04-24 20:26:21 -0500

    register user is ready, styles is missing

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,42 @@
-console.log("Buenas");
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-//Let puede cambiar//
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDFiw5Sz_QQRkEugbvnZC255UdXnCmebDI",
+  authDomain: "pines-lux.firebaseapp.com",
+  projectId: "pines-lux",
+  storageBucket: "pines-lux.appspot.com",
+  messagingSenderId: "598642838534",
+  appId: "1:598642838534:web:8edf18f7d5eb069f84c787"
+};
 
-//Const no cambia, se escriben en mayuscula//
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
-//+ para sumar y concatenar//
-//== valor//
-//=== valor y tipo de dato//
+const registerUserForm = document.getElementById("registerUserForm");
+const auth = getAuth();
 
+registerUserForm.addEventListener("submit", e =>{
+  e.preventDefault();
+
+  const name = registerUserForm.name.value;
+  const lastname = registerUserForm.lastname.value;
+  const email = registerUserForm.email.value;
+  const password = registerUserForm.password.value;
+  registerUser(name, lastname, email, password);
+});
+
+async function registerUser(name, lastname, email, password){
+  try {
+    const { user } = await createUserWithEmailAndPassword(auth,email,password);
+    console.log(user);
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 
