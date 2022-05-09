aa850477b964b505b970cb3c22dfb51aba7f2b0e
erika2201 <erikapapamija@gmail.com>
2022-05-08 21:50:45 -0500

    Add firestore to init collections

--- src/scripts/auth.js
+++ src/scripts/auth.js
@@ -1,18 +1,38 @@
+import { doc, setDoc } from "firebase/firestore";
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
 
 async function registerUser(auth, {email, password}){
     try {
       const { user } = await createUserWithEmailAndPassword(auth,email,password);
-      console.log("registrado");
+      return user;
+
     } catch (e) {
-      console.log(e);
+      if(e.code === "auth/weak-password"){
+        alert("Tu contraseña debe tener al menos 6 caracteres");
+      }
+      if(e.code === "auth/missing-email"){
+        alert("Te falta ingresar un correo");
+      }
+      if(e.code === "auth/email-already-in-use"){
+        alert("Este correo ya está registrado");
+      }
+      console.log(e.code);
     }
   }
 
   async function loginUser(auth, email, password){
     try {
       const { user } = await signInWithEmailAndPassword(auth,email,password);
-      console.log("entré");
+      console.log(user);
+    } catch (e) {
+     alert("Usuario o contraseña inválido");
+    }
+  }
+  
+  async function addUserToDatabase(db, userId, userInfo){
+    try {
+      await setDoc(doc(db, "users", userId), userInfo)
+      
     } catch (e) {
       console.log(e);
     }
@@ -21,4 +41,5 @@ async function registerUser(auth, {email, password}){
   export {
       registerUser,
       loginUser,
+      addUserToDatabase
   }
